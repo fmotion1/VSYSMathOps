06671a6651f4bd62f1121e54688e084cb0f7233a
fmotion1 <futuremotionrecovery@gmail.com>
2024-01-02 13:14:19 -0600

    Remove unnecessary files and build scripts

--- src/VSYSSystemStructs/build_debug.ps1
+++
@@ -1,3 +0,0 @@
-dotnet build .\VSYSSystemStructs.csproj --configuration Debug
-Write-Host -NoNewLine 'Press any key to continue with the operation.'
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
--- src/VSYSSystemStructs/build_release.ps1
+++
@@ -1,3 +0,0 @@
-dotnet build .\VSYSSystemStructs.csproj --configuration Release
-Write-Host -NoNewLine 'Press any key to continue with the operation.'
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
